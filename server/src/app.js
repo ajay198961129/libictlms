66081b7ab25f742175cde4b485bdc80a78d81de8
Ajay Dhiman <86305021+ajay198961129@users.noreply.github.com>
2024-08-25 01:56:10 +0530

    Update app.js

--- server/src/app.js
+++ server/src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const connectDB = require("./config/db");
 const userRoutes = require("./routes/userRoutes");
-const cors = require("cors");
+const cors = require('cors');
 const adminRoutes = require("./routes/adminRoutes");
 
 const app = express();
@@ -9,13 +9,13 @@ const app = express();
 // Connect to MongoDB
 connectDB();
 
-app.use(cors(
-  {
-    origin:["https://www.libict.org"],
-    methods:["POST","GET"],
-    credentials:true
-  }
-));
+app.use(cors({
+  origin: ["https://www.libict.org"],
+  methods: ["POST", "GET"],
+  credentials: true
+}));
+
+
 
 app.get("/",(req,res)=>{
   res.json("hello")
