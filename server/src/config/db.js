b2c058207ad82822d5217bb5617ec7efecabb412
Ajay Dhiman <86305021+ajay198961129@users.noreply.github.com>
2024-08-25 02:59:23 +0530

    Update db.js

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -4,7 +4,7 @@ const connectDB = async () => {
   try {
     // await mongoose.connect("mongodb://localhost:27017/lms-api");
     await mongoose.connect(
-      "mongodb+srv://ajay198961129:E1jv5q42q1vI6tom@cluster0.hm3aw.mongodb.net/libict?retryWrites=true&w=majority&appName=Cluster0"
+      "mongodb+srv://ajay198961129:E1jv5q42q1vI6tom@cluster0.hm3aw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
     );
     console.log("MongoDB connected...");
   } catch (error) {
